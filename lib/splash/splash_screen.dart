a382a937dc33a92754561553feaef9819bc9196e
Brandon <Brandon@DESKTOP-D8SEFDU>
2021-01-30 13:30:48 -0500

    Pushing Luran's changes

--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -8,8 +9,48 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      body: Container(
+        color: Colors.orange[700],
+        alignment: Alignment.center,
+        margin: EdgeInsets.all(60),
+        child: Column(children: [
+          Container(
+            child: Text('The title of this statment'),
+            decoration: BoxDecoration(color: Colors.blue[800]),
+            margin: EdgeInsets.all(230),
+            padding: EdgeInsets.all(70),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 170),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ButtonTheme(
+                    minWidth: 200,
+                    height: 100,
+                    child: RaisedButton(
+                        onPressed: () {
+                          print("Top 5 pressed");
+                        },
+                        child: Text("Search stock tickers"),
+                        color: Colors.blue[800]),
+                  ),
+                  ButtonTheme(
+                      minWidth: 200,
+                      height: 100,
+                      child: RaisedButton(
+                        onPressed: () {
+                          print("Search was pressed");
+                        },
+                        child: Text("Search stock tickers"),
+                        color: Colors.blue[800],
+                      ))
+                ]),
+          ),
+        ]),
+      ),
     );
   }
 }
