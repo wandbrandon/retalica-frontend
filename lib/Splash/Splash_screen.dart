8be34466f5c030e04d585a2711693cc09b6f4b78
luranmanfio0701@gmail.com <luranmanfio0701@gmail.com>
2021-01-30 01:40:52 -0500

    corrected problems with the padding

--- lib/Splash/Splash_screen.dart
+++ lib/Splash/Splash_screen.dart
@@ -9,48 +9,24 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-    
-      child: Text('The app name goes here'),
-      decoration: BoxDecoration(
-        shape: BoxShape.rectangle,
-        color: Colors.blue,
-      ),
-      alignment: Alignment.topCenter,
-      margin: EdgeInsets.all(25.0),
-      padding: EdgeInsets.all(40.0),
-
-      child: Container(
-      child: RaisedButton(
-        onPressed: () {
-          print("top 5 has been pressed");
-        },
-        child: Container(
-          child: Text('5 most talked about stocks'),
-            color: Colors.blueAccent,
-            alignment: Alignment.bottomLeft,
-             margin: EdgeInsets.all(25.0),
-             padding: EdgeInsets.all(40.0),
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      body: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
         ),
+        child: Column(children: [
+          Text("this is the title of the app."),
+          Row(children: [
+            RaisedButton(onPressed: () {
+              print("Top 5 pressed");
+            }),
+            RaisedButton(onPressed: () {
+              print("Search was pressed");
+            })
+          ]),
+        ]),
       ),
     );
-
-     child: Container(
-      child: RaisedButton(
-        onPressed: () {
-          print("search has been pressed");
-        },
-        child: Container(
-          child: Text('search stock tickers'),
-            color: Colors.blueAccent,
-            alignment: Alignment.bottomright,
-             margin: EdgeInsets.all(25.0),
-             padding: EdgeInsets.all(40.0),
-        ),
-      ),
-    );
-    );
-    
   }
 }
-
